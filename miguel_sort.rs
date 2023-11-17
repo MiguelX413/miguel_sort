886fe0d71330c124139bcb8f7f6d58d52245ea3a
MiguelX413 <lbpgaming27@gmail.com>
2023-11-16 19:19:47 -0800

    Remove main from rust impl

--- miguel_sort.rs
+++ miguel_sort.rs
@@ -9,8 +9,3 @@ pub fn miguel_sort(arr: &[u32]) -> Vec<u32> {
         .map(|(i, _)| i as u32)
         .collect()
 }
-
-pub fn main() {
-    println!("{:?}", min_dist(&[1, 5, 10, 6]));
-    println!("{:?}", min_dist(&[1, 5, 7]));
-}

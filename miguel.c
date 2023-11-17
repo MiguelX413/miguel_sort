4d0ac564746288c33f1fea58f50915a2a80bea1c
kyasig <kevinyang177013@gmail.com>
2023-11-17 13:52:44 -0800

    frees the fucking 4294967296 bit array

--- miguel.c
+++ miguel.c
@@ -1,27 +1,28 @@
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 typedef struct {
-  int *arr;
-  size_t size;
-} Sussy;
+    int *arr;
+    size_t size;
+}Sussy;
 
-Sussy miguel_sort(uint32_t *arr, size_t size) {
-  bool *lol = calloc(4294967296, sizeof(bool));
-  Sussy sussy;
-  sussy.arr = malloc(size * sizeof(uint32_t));
-  sussy.size = 0;
-  for (ptrdiff_t i = 0; i < size; i++) {
-    lol[arr[i]] = true;
-  }
-  for (ptrdiff_t i = 0; i < 4294967296; i++) {
-    if (lol[i]) {
-      sussy.arr[sussy.size] = i;
-      sussy.size++;
+Sussy miguel_sort(uint32_t *arr, size_t size){
+    bool* lol = calloc(4294967296, sizeof(bool));
+    Sussy sussy;   
+    sussy.arr = malloc(size*sizeof(uint32_t));
+    sussy.size = 0;
+    for (ptrdiff_t i = 0; i < size; i++){
+        lol[arr[i]] = true;
     }
-  }
-  return sussy;
+    for(ptrdiff_t i = 0; i < 4294967296; i++){
+        if (lol[i]){
+            sussy.arr[sussy.size] = i;
+            sussy.size++;
+        }
+    }
+    free(lol);
+    return sussy;
 }

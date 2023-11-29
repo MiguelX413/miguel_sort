9befa96edb00310df1e33ad9002187b10460cef9
kyasig <92475855+kyasig@users.noreply.github.com>
2023-11-29 13:16:20 -0800

    made tacit
    minor improvements + cleaner point-free style

--- miguel.hs
+++ miguel.hs
@@ -3,11 +3,10 @@ update xs i j = first ++ [j] ++ second
                where first = take i xs
                      second = drop (i + 1) xs
 
-setTrue :: [Int] -> [Bool] -> [Bool]
-setTrue [x] y = update y x True
-setTrue (x:xs) y = setTrue xs updated
-                   where updated = update y x True
+setTrue :: [Bool] -> [Int] -> [Bool]
+setTrue ys [x] = update ys x True
+setTrue ys (x:xs) = setTrue updated xs
+                   where updated = update ys x True
 
 miguelSort :: [Int] -> [Int]
-miguelSort xs = map (\(x,y) -> x) $ filter (\(x, y) -> y) $ zip [0..length lol] lol
-                 where lol = setTrue xs $ take 4294967296 $ repeat False
+miguelSort = map (\(x,_) -> x) . filter (\(_,y) -> y) . zip [0..4294967296] . setTrue (take 4294967296 $ repeat False) 

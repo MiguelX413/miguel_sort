7655af30798dad40a57db4af0f508bfe6e50dfb2
kyasig <92475855+kyasig@users.noreply.github.com>
2023-11-28 16:15:35 -0800

    shitty haskell is slightly less shit
    formatting + generally better code

--- miguel.hs
+++ miguel.hs
@@ -1,13 +1,13 @@
-update::[a] -> Int -> a -> [a]
-update xs i j= first ++ [j] ++ second
-               where first = take (i) xs 
-                     second = drop (i+1) xs
+update :: [a] -> Int -> a -> [a]
+update xs i j = first ++ [j] ++ second
+               where first = take i xs
+                     second = drop (i + 1) xs
 
-set_true:: [Int] -> [Bool] -> [Bool]
-set_true [x] y = update y x True
-set_true (x:xs) y = set_true xs updated
-                    where updated = update y x True
+setTrue :: [Int] -> [Bool] -> [Bool]
+setTrue [x] y = update y x True
+setTrue (x:xs) y = setTrue xs updated
+                   where updated = update y x True
 
-miguel_sort::[Int] -> [Int] 
-miguel_sort xs = map (\x -> x-1) .fst . unzip . filter (\(x, y) -> y == True). zip [1..(length lol)] $ lol
-                 where lol = set_true xs $ take (2^32) $ repeat False
+miguelSort :: [Int] -> [Int]
+miguelSort xs = map (\(x,y) -> x) $ filter (\(x, y) -> y) $ zip [0..length lol] lol
+                 where lol = setTrue xs $ take 4294967296 $ repeat False
